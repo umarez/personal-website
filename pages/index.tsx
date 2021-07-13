0744a6457d76795ab89899c61dda7de5ef53bbd9
umarez <umar.izzuddin@ui.ac.id>
2021-07-13 23:34:08 +0700

    adding responsive on about me

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ export default function Home() {
               Universitas Indonesia student, Software Engineer, IT Enthusiast
             </h1>
           </div>
-          <div className='flex-col translate-y-16 items-center lg:translate-y-40 w-full flex md:flex-row justify-center'>
+          <div className='flex-col translate-y-16  items-center lg:translate-y-40 w-full flex md:flex-row justify-center'>
             <button className='w-32 h-8 lg:w-48 lg:h-14 bg-button text-[#F2ECEC] shadow-shad mb-5 md:mb-0 md:mr-[5.25rem] hover:bg-buttonHover'>
               <h1>About Me</h1>
             </button>
@@ -40,14 +40,14 @@ export default function Home() {
         </div>
       </div>
       <div
-        className='w-[100vw] h-[100vh] relative px-24'
+        className='w-[100vw] h-[100vh] relative px-5 lg:px-32'
         style={{
           background:
             "linear-gradient(180deg, rgba(19, 31, 48, 0.96) 0%, rgba(37, 37, 37, 1) 100%)",
         }}
       >
         <div className='pt-12  text-[#B7B6B6] '>
-          <h1 className='w-full text-center text-[#D9D9DB] text-5xl mb-10'>
+          <h1 className='w-full text-center text-[#D9D9DB] text-3xl md:text-5xl mb-10'>
             About Me
           </h1>
           <div className='w-full flex justify-center '>
@@ -55,7 +55,7 @@ export default function Home() {
               <Image className='' layout='fill' alt='me' src='/me.svg' />
             </div>
           </div>
-          <div className='w-full flex flex-col items-center mt-12 mb-24'>
+          <div className='w-full flex flex-col items-center mt-12 mb-20 md:mb-32'>
             <h1 className=' lg:leading-9 lg:text-xl'>
               Hello, I&apos;m Umar Izzuddin! Universitas Indonesia Computer
               Science&apos;s student who passionate about IT Development.
@@ -65,10 +65,13 @@ export default function Home() {
               driven me to be a guy who never stops learning.
             </h1>
           </div>
-          <div className='flex text-lg items-center justify-between'>
-            <div className='flex items-center'>
-              <h1 className='mr-20'>Connect With me : </h1>
-              <div className='mr-20 cursor-pointer col'>
+          <h1 className='text-center md:hidden font-semibold -translate-y-5'>
+            Connect with me
+          </h1>
+          <div className='flex text-lg items-center justify-center md:justify-between'>
+            <div className='flex items-center justify-center pb-5'>
+              <h1 className='hidden md:block md:mr-20'>Connect With me : </h1>
+              <div className='mr-20 cursor-pointer '>
                 <Image src='/linkedin.svg' width={37} height={37} />
               </div>
               <div className='mr-20 cursor-pointer'>
@@ -78,12 +81,25 @@ export default function Home() {
                 <Image src='/instagram.svg' width={48} height={48} />
               </div>
             </div>
-            <div>
-              <h1>See my experiences</h1>
+            <div className='flex w- cursor-pointer hidden md:flex'>
+              <h1 className='mr-5 '>See my experiences</h1>
+              <Image src='/arrow.svg' width={16} height={16} />
+            </div>
+          </div>
+          <div className='flex justify-center items-center  md:hidden pl-3 '>
+            <div className="cursor-pointer flex items-center">
+            <h1 className='mr-2  font-semibold'>See my experiences</h1>
+            <div className='pt-1' >
+              <Image src='/arrow.svg' width={14} height={14} />
+            </div>
             </div>
           </div>
         </div>
       </div>
+      <div
+        style={{ backgroundColor: "#252525" }}
+        className='w-full h-[100vh]'
+      ></div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   mode: "jit",
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      'mobile-md' : '375px',
+      'mobile-l' : '425px',
+      ...defaultTheme.screens,
+    },
     extend: {
       fontFamily: {
         inconsolata: ["Inconsolata", "monospace"],
@@ -14,6 +21,7 @@ module.exports = {
         button: '#333449',
         buttonHover : '#292B50'
       }
+      
     },
   },
   variants: {
