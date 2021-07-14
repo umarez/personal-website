758ec5fa72c6c285635ea1f2781f0d76e4b5b7a9
umarez <umar.izzuddin@ui.ac.id>
2021-07-14 07:46:01 +0700

    bring back missing components

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,6 +27,17 @@ export default function Home() {
               Universitas Indonesia student, Software Engineer, IT Enthusiast
             </h1>
           </div>
+          <div className='flex-col translate-y-16 items-center lg:translate-y-40 w-full flex md:flex-row justify-center'>
+            <button className='w-32 h-8 lg:w-48 lg:h-14 bg-button text-[#F2ECEC] shadow-shad mb-5 md:mb-0 md:mr-[5.25rem] hover:bg-buttonHover'>
+              <h1>About Me</h1>
+            </button>
+            <button className='w-32 h-8 lg:w-48 lg:h-14 bg-button text-[#F2ECEC] shadow-shad hover:bg-buttonHover'>
+              Projects
+            </button>
+            <button className='w-32 h-8 lg:w-48 lg:h-14 bg-button text-[#F2ECEC] shadow-shad mt-5 md:mt-0 md:ml-[5.25rem] hover:bg-buttonHover'>
+              Contact Me
+            </button>
+          </div>
         </div>
       </div>
       <div
@@ -61,6 +72,15 @@ export default function Home() {
           <div className='flex text-lg items-center justify-center md:justify-between'>
             <div className='flex items-center justify-center pb-5 md:pb-0'>
               <h1 className='hidden md:block md:mr-20'>Connect With me : </h1>
+              <div className='mr-20 cursor-pointer col'>
+                <Image src='/linkedin.svg' width={37} height={37} />
+              </div>
+              <div className='mr-20 cursor-pointer'>
+                <Image src='/github.svg' width={37} height={37} />
+              </div>
+              <div className='cursor-pointer'>
+                <Image src='/instagram.svg' width={48} height={48} />
+              </div>
             </div>
             <div className='flex w- cursor-pointer hidden md:flex'>
               <h1 className='mr-5 '>See my experiences</h1>
@@ -258,8 +278,10 @@ export default function Home() {
                   );
                 })}
               </div>
-              <h1 className='text-2xl text-white pt-10 pb-10'>Coming Soon</h1>
-              <div>
+              <h1 className='text-2xl text-white pt-10 pb-10 text-center'>
+                Coming Soon
+              </h1>
+              <div className='flex flex-col items-center'>
                 <Image
                   src='/e-voting.svg'
                   quality={100}
